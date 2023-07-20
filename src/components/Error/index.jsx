0fa1535c701f15774b617a64741ad35a033349f1
ClemRea <clementrea7@gmail.com>
2023-07-20 12:39:25 +0200

    dernières modif apportés

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,15 +1,11 @@
 import "../../styles/error.css";
 import { Link } from "react-router-dom";
-import Banner from "../banner";
 import "../../styles/utils/tab_resolution/error_media.css";
 import "../../styles/utils/tel_resolution/error_media.css";
 
 function Error() {
   return (
     <div className="kasa_error">
-      <header>
-        <Banner />
-      </header>
       <h1 className="kasa_error_code">404</h1>
       <p className="kasa_error_message">
         Oups! La page que vous demandez n'existe pas.
